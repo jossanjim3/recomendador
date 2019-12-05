4ff8a340c3b8a563490273ca8976d119b980ab0a
jossanjim3 <jossanjim3@gmail.com>
2019-12-05 20:29:09 +0100

    inicio del proyecto con rutas definidas

--- src/index.js
+++ src/index.js
@@ -10,13 +10,13 @@ var app = express();
 
 // Recomendador que devuelva aleatoriamente una lista de 5 peliculas y series 
 // (las que tienes buena puntuacion)
-app.get("recomendador/aleatorio/:userId",(req, res) => {
+app.get("/recomendador/aleatorio/:userId",(req, res) => {
     res.send("<html><body><h1>Aleatorio with film Id...</h1></body></html>");
 });
 
 // Recomendador que devuelva aleatoriamente una lista de hasta NUMBER peliculas y series
 // (las que tienes buena puntuacion)
-app.get("recomendador/aleatorio/:userId/:number",(req, res) => {
+app.get("/recomendador/aleatorio/:userId/:number",(req, res) => {
     res.send("<html><body><h1>Aleatorio with film Id hasta number os films...</h1></body></html>");
 });
 
@@ -31,28 +31,28 @@ app.get("recomendador/aleatorio/:userId/:number",(req, res) => {
 // devuelve una lista de 5 películas de similar categorías que otros usuarios han puntuado
 // sobre una película puntuada. La eleccion de estas peliculas se hace frente a las similaritudes
 // de notacion del usuario con las notas de los otos usuarios.
-app.get("recomendador/porSimilitudes/pelicula/:filmId/:userId",(req, res) => {
+app.get("/recomendador/porSimilitudes/pelicula/:filmId/:userId",(req, res) => {
     res.send("<html><body><h1>Similitudes with film Id and user Id...</h1></body></html>");
 });
 
 // devuelve una lista de hasta películas, de similar categorías que otros usuarios han puntuado 
 // sobre una película puntuada. La eleccion de estas peliculas se hace frente a las similaritudes 
 // de notacion del usuario con las notas de los otos usuarios.
-app.get("recomendador​/porSimilitudes​/pelicula​/:filmId}/:userId/:number",(req, res) => {
+app.get("/recomendador​/porSimilitudes​/pelicula​/:filmId}/:userId/:number",(req, res) => {
     res.send("<html><body><h1>Similitudes with film Id and user Id with max number...</h1></body></html>");
 });
 
 // devuelve una lista de 5 películas, o series, de similar categorías que otros usuarios 
 // han puntuado sobre una película puntuada. La eleccion de estas peliculas, o series, se hace 
 // frente a las similaritudes de notacion del usuario con las notas de los otos usuarios.
-app.get("recomendador/porSimilitudes/serie/:serieId/:userId",(req, res) => {
+app.get("/recomendador/porSimilitudes/serie/:serieId/:userId",(req, res) => {
     res.send("<html><body><h1>Similitudes with serie Id and user Id...</h1></body></html>");
 });
 
 // devuelve una lista de hasta películas, o series, de similar categorías que otros usuarios han 
 // puntuado sobre una película puntuada. La eleccion de estas peliculas, o series, se hace frente a 
 // las similaritudes de notacion del usuario con las notas de los otos usuarios.
-app.get("recomendador/porSimilitudes/serie/:serieId/:userId/:number",(req, res) => {
+app.get("/recomendador/porSimilitudes/serie/:serieId/:userId/:number",(req, res) => {
     res.send("<html><body><h1>Similitudes with serie Id and user Id with max number......</h1></body></html>");
 });
 
