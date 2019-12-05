afdd3857136c12b89849a026c8d383f7690a99ce
jossanjim3 <jossanjim3@gmail.com>
2019-12-05 18:20:48 +0100

    commit inicial

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var port = 3000;
+
+console.log("Starting API server...");
+
+var app = express();
+
+app.get("/",(req, res) => {
+    res.send("<html><body><h1>Recomendador Server...</h1></body></html>");
+});
+
+app.listen(port);
+
+console.log("Server ready");
