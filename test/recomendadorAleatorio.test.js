3ed9ca26855bd2575384a10ad2e18399f618e8ec
jossanjim3 <jossanjim3@gmail.com>
2020-01-04 10:38:07 +0100

    ya usa el mock generado para las pruebas test del recomendador aleatorio de peliculas y series

--- test/recomendadorAleatorio.test.js
+++ test/recomendadorAleatorio.test.js
@@ -8,22 +8,23 @@ const obtenerSeriesAleatoriasTmdb = require('../src/recomendador').obtenerSeries
 
 const API_KEY = '?api_key=18268e82edbd92497a6d18853ddf8c57';
 const LANGUAGE = "&language=es-ES";
+const PAGE = "&page=1"
 const EXT_SOURCE = "&external_source=imdb_id";
 const AUTHENTIFICATION_TOKEN = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dpbiI6ImFkbWluIiwiaWF0IjoxNTc3OTg5MzgwLCJleHAiOjE1Nzc5OTI5ODB9.8HQaMFCpyvdpd4csrvnwv5PkMkrmPu_zg5rSq119xbY";
 
 function mockPeliculasSet1() {
-    nock('http://localhost:3000/recomendador/v1')
-            .get('/aleatorio/peliculas' + API_KEY + LANGUAGE)
+    nock('https://api.themoviedb.org/3/movie')
+            .get('/popular' + API_KEY + LANGUAGE + PAGE)
             .reply(200, peliculasSet1);
 }
 
 function mockSeriesSet1() {
-    nock('http://localhost:3000/recomendador/v1')
-            .get('/aleatorio/series' + API_KEY + LANGUAGE)
+    nock('https://api.themoviedb.org/3/tv')
+            .get('/popular' + API_KEY + LANGUAGE + PAGE)
             .reply(200, seriesSet1);
 }
 
-jest.setTimeout(30000);
+//jest.setTimeout(30000);
 
 describe("Recomendador Aleatorio Test API", () => {
 
@@ -34,9 +35,9 @@ describe("Recomendador Aleatorio Test API", () => {
             //expect an object back
             expect(typeof response).to.equal('object');
     
-            expect(response[0].id).to.equal(419704);
-            //expect(response[0].popularity).to.equal(590.821);
-            expect(response[0].original_title ).to.include.any.string("Ad","Astra");
+            expect(response[0].id).to.equal(181812);
+            expect(response[0].popularity).to.equal(454.192);
+            expect(response[0].original_title ).to.include.any.string("Star","Wars", "Skywalker");
           });
     });
 
@@ -48,7 +49,7 @@ describe("Recomendador Aleatorio Test API", () => {
             expect(typeof response).to.equal('object');
     
             expect(response[0].id).to.equal(57243);
-            //expect(response[0].popularity).to.equal(413.45);
+            expect(response[0].popularity).to.equal(413.45);
             expect(response[0].original_name).to.include.any.string("Doctor");
           });
     });
